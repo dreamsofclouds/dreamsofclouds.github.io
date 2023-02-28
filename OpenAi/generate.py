d9f8b4ef031a19348cada0cb5157fac5f4e9e66d
Daniel LV <daniel.laiton.veloza@gmail.com>
2023-02-28 14:15:45 +0100

    OpenAI Scripts and install

---
+++ OpenAi/generate.py
@@ -0,0 +1,20 @@
+import openai
+from PIL import Image
+import requests
+
+# Load your API key from an environment variable or secret management service
+openai.api_key = "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"
+
+
+response = openai.Image.create(
+  prompt="Dancing Witches. Anamorphosis. Baphomet",
+  n=5,
+  size="1024x1024"
+)
+
+for image_data in response['data']:
+    image_url = image_data['url']
+    image = Image.open(requests.get(image_url, stream=True).raw)
+    image.show()
+
+
