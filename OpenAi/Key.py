bf7cfa0ad3a1edaf7156f9554e857d03e19aaf33
Daniel LV <daniel.laiton.veloza@gmail.com>
2023-03-03 10:01:44 +0100

    Changed the API KEY and Added Musclewiki

--- OpenAi/Key.py
+++ OpenAi/Key.py
@@ -2,6 +2,6 @@ import os
 import openai
 
 # Load your API key from an environment variable or secret management service
-openai.api_key = "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"
+openai.api_key = "OPENAI_API_KEY"
 
 response = openai.Completion.create(model="text-davinci-003", prompt="Say this is a test", temperature=0, max_tokens=7)
--- OpenAi/generate.py
+++ OpenAi/generate.py
@@ -3,7 +3,7 @@ from PIL import Image
 import requests
 
 # Load your API key from an environment variable or secret management service
-openai.api_key = "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"
+openai.api_key = "OPENAI_API_KEY"
 
 
 response = openai.Image.create(
