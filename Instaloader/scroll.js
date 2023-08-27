0a0a830debe65138c1d9b95ab0a0e43bd097c205
dreamsofclouds <daniel.laiton.veloza@gmail.com>
2023-08-27 10:11:42 +0200

    script Instaloader

---
+++ Instaloader/scroll.js
@@ -0,0 +1,26 @@
+// Set the duration (in milliseconds) for each scroll step
+const scrollDuration = 1000;
+
+// Set the scroll step size (in pixels)
+const scrollStep = window.innerHeight;
+
+// Function to scroll the website automatically
+function autoScroll() {
+  // Get the current scroll position
+  let currentPosition = window.pageYOffset || document.documentElement.scrollTop;
+
+  // Calculate the target position to scroll to
+  const targetPosition = currentPosition + scrollStep;
+
+  // Check if we've reached the bottom of the page
+  if (targetPosition >= document.body.scrollHeight) {
+    clearInterval(scrollInterval); // Stop scrolling
+    return;
+  }
+
+  // Scroll to the next position
+  window.scrollTo(0, targetPosition);
+}
+
+// Start scrolling automatically with the specified interval
+const scrollInterval = setInterval(autoScroll, 5000); // 5 seconds (5000 milliseconds)
