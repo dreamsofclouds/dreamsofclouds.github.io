2b29c06325daad3a480e9585ed9d323930f24a4f
Daniel LV <daniel.laiton.veloza@gmail.com>
2023-11-13 16:25:02 +0100

    Test

--- Course/BoardGameManuals/scripts.js
+++ Course/BoardGameManuals/scripts.js
@@ -1,55 +1,39 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Fetch PDF files from the 'Manuals' folder
-    fetchPDFFiles().then(boardGames => {
-        const manualsList = document.getElementById('manuals-list');
+    // Replace the following array with your actual data (game names and PDF URLs)
+    const boardGames = [
+        { name: 'Catan', pdfUrl: 'catan_base_rules_2020_200707.pdf' },
+        { name: 'Scythe', pdfUrl: 'Manuals/catan_base_rules_2020_200707.pdf' },
+        // Add more games as needed
+    ];
 
-        boardGames.forEach(game => {
-            console.log('Processing game:', game.name);
 
-            const manualItem = document.createElement('div');
-            manualItem.className = 'manual-item';
+    const manualsList = document.getElementById('manuals-list');
+    
+    boardGames.forEach(game => {
+        console.log('Processing game:', game.name);
+        console.log('adding a link for:', game.pdfUrl);
+        const manualItem = document.createElement('div');
+        manualItem.className = 'manual-item';
 
-            const gameName = document.createElement('h2');
+        const gameName = document.createElement('h2');
 
-            // Create a link for the game name
-            const gameLink = document.createElement('a');
-            gameLink.href = game.pdfUrl; // Link to the PDF
-            gameLink.textContent = game.name;
-            gameLink.target = '_blank'; // Open link in a new tab
+        //Create a link for the game name
+        const gameLink = document.createElement('a');
+        gameLink.href = game.pdfUrl; // Link to the PDF
+        gameLink.textContent = game.name;
+        gameLink.target = '_blank'; // Open link in a new tab
 
-            gameName.appendChild(gameLink);
+        gameName.appendChild(gameLink);
 
-            const qrCode = document.createElement('img');
-            qrCode.className = 'qr-code';
-            const qrCodeUrl = generateQRCode(game.pdfUrl);
-            qrCode.src = qrCodeUrl;
-            qrCode.alt = `QR Code for ${game.name}`;
+        const qrCode = document.createElement('img');
+        qrCode.className = 'qr-code';
+        qrCode.src = generateQRCode(game.pdfUrl);
+        qrCode.alt = `QR Code for ${game.name}`;
 
-            console.log('Generated QR Code URL:', qrCodeUrl);
-
-            manualItem.appendChild(gameName);
-            manualItem.appendChild(qrCode);
-            manualsList.appendChild(manualItem);
-        });
+        manualItem.appendChild(gameName);
+        manualItem.appendChild(qrCode);
+        manualsList.appendChild(manualItem);
     });
-
-    async function fetchPDFFiles() {
-        // Assume 'Manuals' is in the same directory as the HTML file
-        const response = await fetch('./Manuals/');
-        const html = await response.text();
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(html, 'text/html');
-
-        const links = doc.querySelectorAll('a');
-        const pdfFiles = Array.from(links)
-            .map(link => link.href)
-            .filter(href => href.endsWith('.pdf'));
-
-        return pdfFiles.map(pdfUrl => ({
-            name: pdfUrl.substring(pdfUrl.lastIndexOf('/') + 1, pdfUrl.lastIndexOf('.pdf')),
-            pdfUrl: pdfUrl
-        }));
-    }
 
     function generateQRCode(url) {
         // Log the URL to check if it's correct
