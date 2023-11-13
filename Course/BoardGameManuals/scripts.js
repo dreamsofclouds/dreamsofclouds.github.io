ad24ef026b33f5294fa329153bc576630225c032
dreamsofclouds <daniel.laiton.veloza@gmail.com>
2023-11-13 22:39:44 +0100

    further Testing of the qrious API

--- Course/BoardGameManuals/scripts.js
+++ Course/BoardGameManuals/scripts.js
@@ -1,23 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Replace the following array with your actual data (game names and PDF URLs)
     const boardGames = [
-        { name: 'Catan', pdfUrl: 'catan_base_rules_2020_200707.pdf' },
-        { name: 'Scythe', pdfUrl: 'Manuals/catan_base_rules_2020_200707.pdf' },
+        { name: 'Catan', pdfUrl: 'Manuals/Catan.pdf' },
+        { name: 'Scythe', pdfUrl: 'Manuals/Scythe.pdf' },
         // Add more games as needed
     ];
 
-
     const manualsList = document.getElementById('manuals-list');
-    
+
     boardGames.forEach(game => {
         console.log('Processing game:', game.name);
-        console.log('adding a link for:', game.pdfUrl);
+        
         const manualItem = document.createElement('div');
         manualItem.className = 'manual-item';
 
         const gameName = document.createElement('h2');
-
-        //Create a link for the game name
+        
+        // Create a link for the game name
         const gameLink = document.createElement('a');
         gameLink.href = game.pdfUrl; // Link to the PDF
         gameLink.textContent = game.name;
@@ -25,22 +24,24 @@ document.addEventListener('DOMContentLoaded', function () {
 
         gameName.appendChild(gameLink);
 
-        const qrCode = document.createElement('img');
+        const qrCode = document.createElement('canvas');
         qrCode.className = 'qr-code';
-        qrCode.src = generateQRCode(game.pdfUrl);
-        qrCode.alt = `QR Code for ${game.name}`;
+        generateQRCode(game.pdfUrl, qrCode);
 
         manualItem.appendChild(gameName);
         manualItem.appendChild(qrCode);
         manualsList.appendChild(manualItem);
     });
 
-    function generateQRCode(url) {
+    function generateQRCode(url, element) {
         // Log the URL to check if it's correct
         console.log('Generating QR Code for URL:', url);
-
-        // In a real-world scenario, you would use a QR code generation library.
-        // For simplicity, this example uses a placeholder image.
-        return 'https://dummyimage.com/100x100/000/fff&text=QR+Code';
+    
+        // Create a new QRCode instance
+        const qr = new QRCode(element, {
+            text: url,
+            width: 100, // Adjust the width as needed
+            height: 100, // Adjust the height as needed
+        });
     }
 });
